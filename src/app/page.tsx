00d79b6697d2e2202b5e025f9f12c3b07f334a5e
Daich1 <mrdaichi29@gmail.com>
2025-10-01 01:00:33 +0900

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-[#0d0d0d]">
+      <h1 className="text-5xl font-bold text-[#f0558b]">
+        Hello Nelo 🚀
+      </h1>
+    </main>
+  );
+}
