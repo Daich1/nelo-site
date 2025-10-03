8266653c9ca259b9e2325fe5d79d909be5a1cecc
Daich1 <mrdaichi29@gmail.com>
2025-10-04 06:26:34 +0900

    feat(auth): make all pages require login

--- middleware.ts
+++ middleware.ts
@@ -1,8 +1,8 @@
+// middleware.ts
 export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    // /signin は除外して、それ以外は全部保護
-    "/((?!api|_next|signin).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|signin).*)",
   ],
 };
